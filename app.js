84716de1ceb38d635e05e08db2dcf9176469d277
Adanurk <gumussoykbr@gmail.com>
2022-09-26 00:21:30 +0200

    add

--- app.js
+++ app.js
@@ -2,4 +2,20 @@ const express = require('express');
 const app = express();
 //adds a bunch of functions to our app variable
 
-app.listen();
+//routing
+//* to determine how an application response to certain client requests/to certain url or http methods
+
+app.get('/', (req, res) => {
+  res
+    .status(200)
+    .json({ message: 'Hello from the server side!', app: 'Natours' });
+});
+
+app.post('/', (req, res) => {
+  res.send('You can post to this endpoint...');
+});
+
+const port = 3000;
+app.listen(port, () => {
+  console.log(`App running on port ${port}...`);
+});
