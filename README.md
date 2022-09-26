c582cfee6f6e7003d0b1f1f103ee74a48241769c
Adanurk <gumussoykbr@gmail.com>
2022-09-26 13:09:43 +0200

    in progress

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # natours-project
+
+It is an application where we can see tours, also book them, we can create user accounts and logged in. We have to log in before we book.
--- app.js
+++ app.js
@@ -1,18 +1,42 @@
+const fs = require('fs');
 const express = require('express');
 const app = express();
 //adds a bunch of functions to our app variable
 
+app.use(express.json());
+//* with this method we make sure that the data from user will be saved in request object
+// app.use(express.urlencoded({ extended: true }));
 //routing
 //* to determine how an application response to certain client requests/to certain url or http methods
 
-app.get('/', (req, res) => {
-  res
-    .status(200)
-    .json({ message: 'Hello from the server side!', app: 'Natours' });
+// app.get('/', (req, res) => {
+//   res
+//     .status(200)
+//     .json({ message: 'Hello from the server side!', app: 'Natours' });
+// });
+
+// app.post('/', (req, res) => {
+//   res.send('You can post to this endpoint...');
+// });
+
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
+);
+
+app.get('/api/v1/tours', (req, res) => {
+  res.status(200).json({
+    status: 'success',
+    results: tours.length,
+    data: {
+      tours,
+    },
+  });
 });
 
-app.post('/', (req, res) => {
-  res.send('You can post to this endpoint...');
+app.post('/api/v1/tours', (req, res) => {
+  console.log(req.body);
+  res.send('Done');
+  //we always have to send back sth in order to complete request
 });
 
 const port = 3000;
