a722beddf42ee7d668d03231054b9acbdd874a74
Adanurk <gumussoykbr@gmail.com>
2022-09-27 18:35:55 +0200

    post added

--- app.js
+++ app.js
@@ -34,8 +34,25 @@ app.get('/api/v1/tours', (req, res) => {
 });
 
 app.post('/api/v1/tours', (req, res) => {
-  console.log(req.body);
-  res.send('Done');
+  // console.log(req.body);
+  const newId = tours[tours.length - 1].id + 1;
+  const newTour = Object.assign({ id: newId }, req.body);
+  //object assign here allows us to combine two objects and gives one object, by merging them
+  tours.push(newTour);
+  fs.writeFile(
+    `${__dirname}/dev-data/data/tours-simple.json`,
+    JSON.stringify(tours),
+    (err) => {
+      res.status(201).json({
+        status: 'success',
+        data: {
+          tour: newTour,
+        },
+      });
+      //201 means created
+    }
+  );
+  //since we are in now a callback function and thats why we are running now in event loop, we shouldnt block the event loop so we have to use async version of the method here!
   //we always have to send back sth in order to complete request
 });
 
